79d7321d357417038ac71370e10d883a4b53cc57
Nipu Kumari <nipu.c7@gmail.com>
2025-11-11 12:54:26 +0530

    v1

--- src/test/java/com/driver/test/Main.java
+++ src/test/java/com/driver/test/Main.java
@@ -1,22 +1,6 @@
 package com.driver.test;
 
-
 public class Main {
-
-    public static class Product {
-        public int product(int x, int y) {
-            return x * y;
-        }
-
-        public int product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        public double product(double x, double y) {
-            return x * y;
-        }
-    }
-
     public static void main(String[] args) {
         Product p = new Product();
         p.product(5, 3);

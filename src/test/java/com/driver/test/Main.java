8a93591357c8b4d9990e4b852bebbb2de209a0c7
Nipu Kumari <nipu.c7@gmail.com>
2025-11-11 12:26:37 +0530

    sol1

---
+++ src/test/java/com/driver/test/Main.java
@@ -0,0 +1,28 @@
+package com.driver.test;
+
+public class Main {
+
+    public static class Product {
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Product p = new Product();
+
+        p.product(5, 3);
+        p.product(2, 3, 4);
+        p.product(2.5, 3.5);
+    }
+}
